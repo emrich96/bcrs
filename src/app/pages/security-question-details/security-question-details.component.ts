ab2654273dc1e233c9857d7410539b1325edf483
Mike Goldberg <michael@ezlandlordforms.com>
2020-11-02 21:48:35 -0800

    update security question

--- src/app/pages/security-question-details/security-question-details.component.ts
+++ src/app/pages/security-question-details/security-question-details.component.ts
@@ -9,12 +9,11 @@
 */
 
 
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SecurityQuestionServiceService } from './../../shared/security-question.service.service';
 import { SecurityQuestion } from './../../shared/security-question.interface';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-security-question-details',
@@ -23,45 +22,31 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SecurityQuestionDetailsComponent implements OnInit {
 
-  question: SecurityQuestion;
-  questionId: string;
-  form: FormGroup;
+  questionData: SecurityQuestion;
+  questionForm: FormGroup;
 
   // capture and place questionId, set value of selected questionId
   // Submit and cancel button in dialog box
   // tslint:disable-next-line: max-line-length
-  constructor(private route: ActivatedRoute, private http: HttpClient, private fb: FormBuilder, private router: Router, private securityQuestionServiceService: SecurityQuestionServiceService) {
-    this.questionId = this.route.snapshot.paramMap.get('questionId');
-
-    this.securityQuestionServiceService.findAllSecurityQuestionsById(this.questionId).subscribe(res => {
-      this.question = res.data;
-    }, err => {
-      console.log(err);
-    }, () => {
-      this.form.controls.text.setValue(this.question.text);
-    });
+  constructor(private fb: FormBuilder,private dialogRef: MatDialogRef<SecurityQuestionDetailsComponent>, @Inject(MAT_DIALOG_DATA) data) {
+    this.questionData = data.questionData
   }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
-    this.form = this.fb.group({
-      text: [null, Validators.compose([Validators.required])],
-    });
+    console.log(this.questionData.text)
+    this.questionForm = new FormGroup({
+      text: new FormControl(this.questionData.text, Validators.required)
+    })
   }
 
-  // tslint:disable-next-line: typedef
-  saveQuestion() {
-    const updatedSecurityQuestion = {} as SecurityQuestion;
-    updatedSecurityQuestion.text = this.form.controls.text.value;
-
-    this.securityQuestionServiceService.updateSecurityQuestionsById(this.questionId, updatedSecurityQuestion).subscribe(res => {
-      this.router.navigate(['/security-questions']);
-    });
+  updateUser() {
+    // pass the updated values to back to the parent
+    this.dialogRef.close(this.questionForm.value);
   }
 
-  // tslint:disable-next-line: typedef
   cancel() {
-    this.router.navigate(['/security-questions']);
+    this.dialogRef.close();
   }
 
 }
