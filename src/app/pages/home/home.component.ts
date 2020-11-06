36b463746bc38da393860fe1ed217048bc1404ac
ashleigh-lyman <ajlyman@my365.bellevue.edu>
2020-11-05 21:09:07 -0600

    added new files and added ts content to files

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,10 @@
 */
 
 import { Component, OnInit } from '@angular/core';
+import { ServiceRepairService } from '../../shared/services/service-repair.service';
+import { LineItem } from '../../shared/interfaces/line-item.interface';
+import { Invoice } from '../../shared/interfaces/invoice.interface';
+import { InvoiceService } from '../../shared/services/invoice.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -20,10 +24,89 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  form: FormGroup;
+  userName: string;
+  services: ServiceRepairItem[];
+  lineItem: LineItem[];
 
-  constructor() { }
+  constructor(private cookieService: CookieService,
+    private fb: FormBuilder, private dialog: MatDialog, private router: Router,
+    private serviceRepairService: ServiceRepairService, private invoiceService: InvoiceService) {
 
-  ngOnInit(): void {
+    this.userName = this.cookieService.get('sessionuser');
+    this.services = this.serviceRepairService.getServiceRepairItem();
   }
 
+  ngOnInit(): void {
+
+    this.form = this.fb.group({
+      parts: [null, Validators.compose([Validators.required])],
+      labor: [null, Validators.compose([Validators.required])],
+      alternator: [null, null]
+    });
+  }
+
+  submit(form) {
+    console.log(form);
+    const selectedServiceIds = [];
+    for (const [key, value] of Object.entries(form.checkGroup)) {
+      if (value) {
+        selectedServiceIds.push({
+          id: key
+        });
+      }
+    }
+    this.lineItems = [];
+    /*Build  the invoice object
+    */
+
+    for (const savedService of this.services) {
+      for (const selectedService of selectedServiceIds) {
+        if (savedService.id === selectedService.id) {
+          this.lineItems.push({
+            title: savedService.title,
+            price: savedService.price
+          });
+        }
+      }
+    }
+    console.log(this.lineItems);
+
+    const partsAmount = parseFloat(form.parts);
+    const laborAmount = form.labor = 50;
+    const lineItemTotal = this.lineItems.reduce({ prev, cur) => prev + cur.price, 0);
+    const total = partsAmount + laborAmount + lineItemTotal;
+
+    const invoice = {
+      userName: this.userName,
+      lineItems: this.lineItems,
+      partsAmount: partsAmount,
+      laborAmount: laborAmount,
+      lineItemTotal: lineItemTotal,
+      total: total,
+      orderDate: new Date()
+    } as Invoice;
+
+    console.log(invoice);
+
+    const dialogRef = this.dialog.open(InvoiceSummaryDialogComponent, {
+      data: {
+        invoice: invoice
+      },
+      disableClose: true,
+      width: '800px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'confirm') {
+        console.log('Invoice saved');
+
+        this.invoiceService.createInvoice(invoice.userName, invoice).subscribe(res => {
+          this.router.navigate(['/']);
+        }, err => {
+          console.log(err);
+        })
+      }
+    });
+  }
 }
--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -12,6 +12,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Role } from '../../shared/interfaces/role.interface';
 import { User } from 'src/app/shared/interfaces/user.interface';
 
 @Component({
@@ -22,6 +23,7 @@ import { User } from 'src/app/shared/interfaces/user.interface';
 export class UserDetailsComponent implements OnInit {
   userData: User;
   userForm: FormGroup
+  roles: Role[];
 
   constructor(private fb: FormBuilder,private dialogRef: MatDialogRef<UserDetailsComponent>, @Inject(MAT_DIALOG_DATA) data) {
     this.userData = data.userData
